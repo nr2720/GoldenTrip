1a10d1b7dfbd6c128a7eafa4aca2f38816850990
nr2720 <125583236+nr2720@users.noreply.github.com>
2024-04-25 04:12:13 -0400

    Keep in touch mail added

--- src/componants/GetIn.jsx
+++ src/componants/GetIn.jsx
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+
+
 function GetIn() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        if(e.target.type === 'email') {
+            setEmail(value);
+        }
+        else if(e.target.type === 'text') {
+            setName(value);
+        }
+        else{
+            return;
+        }
+    }
+
     return(
         <div className="getIn">
-            <button className="buttonGold">Get in</button>
+            <h1>We'll wait for you, {name}</h1>
+            <form>
+            <input type="text" placeholder="Name" onChange={handleChange} value={name}/>
+            <input type="email" placeholder="Email Address" onChange={handleChange} value={email}/>
+            <input type="submit" value="Subscribe"/>
+            </form>
         </div>
     )
 }
